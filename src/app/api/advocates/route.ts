93a919801ba92c03e69dfa4c5ff50f36a9c8ae74
Stephen Watzman <stephenwatzman@Stephens-MacBook-Pro-2.local>
2025-03-19 11:16:25 -0400

    feat: moved pagination and filtering to the database layer and updated the frontend to reflect that

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,51 @@
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
+import { NextRequest } from "next/server";
+import { sql } from "drizzle-orm";
 
-export async function GET() {
-  // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const page = Number(searchParams.get("page")) || 1;
+  const pageSize = Number(searchParams.get("pageSize")) || 20;
+  const searchTerm = searchParams.get("searchTerm") || "";
 
-  const data = advocateData;
+  try {
+    const offset = (page - 1) * pageSize;
 
-  return Response.json({ data });
+    const advocateResult = await db
+      .select()
+      .from(advocates)
+      .where(
+        sql`first_name ILIKE ${`%${searchTerm}%`} OR
+            last_name ILIKE ${`%${searchTerm}%`} OR 
+            city ILIKE ${`%${searchTerm}%`} OR 
+            degree ILIKE ${`%${searchTerm}%`} OR 
+            phone_number::text ILIKE ${`%${searchTerm}%`} OR 
+            payload::text ILIKE ${`%${searchTerm}%`}`
+      )
+      .limit(pageSize)
+      .offset(offset);
+
+    const totalCountResult = await db
+      .select()
+      .from(advocates)
+      .where(
+        sql`first_name ILIKE ${`%${searchTerm}%`} OR
+            last_name ILIKE ${`%${searchTerm}%`} OR
+            city ILIKE ${`%${searchTerm}%`} OR
+            degree ILIKE ${`%${searchTerm}%`} OR
+            phone_number::text ILIKE ${`%${searchTerm}%`} OR
+            payload::text ILIKE ${`%${searchTerm}%`}`
+      );
+
+    const totalCount = totalCountResult.length;
+
+    return Response.json({
+      advocates: advocateResult,
+      totalCount: totalCount,
+    });
+  } catch (error) {
+    console.error("Error fetching advocates:", error);
+    return Response.json({ advocates: [], totalCount: 0 });
+  }
 }
